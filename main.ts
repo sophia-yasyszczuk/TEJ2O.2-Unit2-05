1925a293aafbc3147bd65b8d96085d3caa55884f
sophia-yasyszczuk <sophia.yasyszczuk@stu.ocsb.ca>
2026-02-27 13:20:15 -0500

    Made microbit say temperature in Kelvin

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Sophia
+ * Created on: Feb 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let temperatureInKelvin: number
+let temperatureInCelcius: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function () {
+    temperatureInCelcius = input.temperature()
+    temperatureInKelvin = temperatureInCelcius+273.15
+
+    basic.clearScreen()
+    basic.showString("the temperature is:" + temperatureInKelvin + " K.")
+})
